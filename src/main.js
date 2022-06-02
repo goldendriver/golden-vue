1c11d7480b897c19c0bb646177d4f1f0c9932bba
golden <goldendriver0731@gmail.com>
2022-06-02 16:37:03 +0900

    directive check

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const app = createApp(App);
+// const app = createApp({
+// 	data() {
+// 		return {
+// 			count: 0
+// 		}
+// 	}
+// });
+
+app.mount('#app')
